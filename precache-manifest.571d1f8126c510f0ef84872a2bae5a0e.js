a97a201a3482483fc25627f3da82d8539e2fea92
Anastasia Buterina <anastasia@buterina.com>
2022-07-28 11:35:21 -0400

    Updates

---
+++ precache-manifest.571d1f8126c510f0ef84872a2bae5a0e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "96ceca6ee25cd0d7f49e",
+    "url": "/mesto-react/static/css/main.26cc539c.chunk.css"
+  },
+  {
+    "revision": "96ceca6ee25cd0d7f49e",
+    "url": "/mesto-react/static/js/main.b4a3c5ec.chunk.js"
+  },
+  {
+    "revision": "143dc8e548ccab3a4196",
+    "url": "/mesto-react/static/js/runtime~main.a8a5e490.js"
+  },
+  {
+    "revision": "c4343937ac6402314f77",
+    "url": "/mesto-react/static/js/2.ca047ad3.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/mesto-react/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/mesto-react/static/media/inter-400.dc131113.woff2"
+  },
+  {
+    "revision": "75db5319e7e87c587019a5df08d7272c",
+    "url": "/mesto-react/static/media/inter-500.75db5319.woff2"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "/mesto-react/static/media/inter-500.c0638bea.woff"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/mesto-react/static/media/inter-400.3ac83020.woff"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/mesto-react/static/media/inter-900.661569af.woff2"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/mesto-react/static/media/inter-900.d0b121f3.woff"
+  },
+  {
+    "revision": "e2e95a1709cdd7265876927fdff95930",
+    "url": "/mesto-react/static/media/button-close.e2e95a17.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/mesto-react/static/media/button-edit.6bfdb354.svg"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/mesto-react/static/media/button-plus.b2fce4ac.svg"
+  },
+  {
+    "revision": "00b1c22a19c7a1e64f19b1fbacc3a301",
+    "url": "/mesto-react/static/media/button-heart-disabled.00b1c22a.svg"
+  },
+  {
+    "revision": "3d813f810a41032c140568c878fd7d85",
+    "url": "/mesto-react/static/media/button-heart-full.3d813f81.svg"
+  },
+  {
+    "revision": "4220563aa7e7f6bb1d562a95174678b6",
+    "url": "/mesto-react/static/media/button-trash.4220563a.svg"
+  },
+  {
+    "revision": "bb94eb64043c60c70a612d2f1587c564",
+    "url": "/mesto-react/static/media/places-logo.bb94eb64.png"
+  },
+  {
+    "revision": "543d91c30cc5464736078d1841c520a2",
+    "url": "/mesto-react/index.html"
+  }
+];
